d9d44fcb535102792b45448d93bc80147f65aec8
Kyumin Kim <math4tots@users.noreply.github.com>
2019-02-03 23:14:20 -0800

    USACO: factor out some boilerplate into _testutil.py

---
+++ mtots/usaco/_testutil.py
@@ -0,0 +1,45 @@
+"""
+Utility for testing USACO problems
+"""
+
+
+class FakeFile:
+    def __init__(self, mode, data):
+        assert isinstance(data, str)
+        self.mode = mode
+        self.contents = [data]
+
+    def write(self, data):
+        assert isinstance(data, str), repr(data)
+        assert self.mode == 'w', self.mode
+        self.contents.append(data)
+
+    def read(self):
+        return ''.join(self.contents)
+
+
+def case(main, inputs, outputs):
+    from mtots import test
+    import contextlib
+
+    contents = dict(inputs)
+
+    @contextlib.contextmanager
+    def open(name, mode='r'):
+        if mode == 'r':
+            yield FakeFile('r', contents[name])
+        elif mode == 'w':
+            fake_file = FakeFile('w', '')
+            yield fake_file
+            contents[name] = fake_file.read()
+        else:
+            assert False, mode
+
+    main(open)
+
+    for filename in outputs:
+        test.that(filename in contents, f'File {filename} missing')
+        test.equal(
+            f'FILE{filename}:{contents[filename]}',
+            f'FILE{filename}:{outputs[filename]}',
+        )
--- mtots/usaco/milk3.py
+++ mtots/usaco/milk3.py
@@ -47,73 +47,26 @@ def solve(A, B, C):
 
 if __name__ == '__main__':
     main(open)
+else:
+    from mtots import test
+    from . import _testutil
 
-
-def _sample():
-    _t({
+    @test.case
+    def _sample():
+        _testutil.case(main, {
 'milk3.in': """8 9 10
 """
-    }, {
+        }, {
 'milk3.out': """1 2 8 9 10
 """,
-    })
+        })
 
-
-def _sample2():
-    _t({
+    @test.case
+    def _sample2():
+        _testutil.case(main, {
 'milk3.in': """2 5 10
 """
-    }, {
+        }, {
 'milk3.out': """5 6 7 8 9 10
 """,
-    })
-
-
-def _t(inputs, outputs):
-    from mtots import test
-    import contextlib
-
-    contents = dict(inputs)
-
-    @contextlib.contextmanager
-    def open(name, mode='r'):
-        if mode == 'r':
-            yield FakeFile('r', contents[name])
-        elif mode == 'w':
-            fake_file = FakeFile('w', '')
-            yield fake_file
-            contents[name] = fake_file.read()
-        else:
-            assert False, mode
-
-    main(open)
-
-    for filename in outputs:
-        test.that(filename in contents, f'File {filename} missing')
-        test.equal(
-            f'FILE{filename}:{contents[filename]}',
-            f'FILE{filename}:{outputs[filename]}',
-        )
-
-
-class FakeFile:
-    def __init__(self, mode, data):
-        assert isinstance(data, str)
-        self.mode = mode
-        self.contents = [data]
-
-    def write(self, data):
-        assert isinstance(data, str), repr(data)
-        assert self.mode == 'w', self.mode
-        self.contents.append(data)
-
-    def read(self):
-        return ''.join(self.contents)
-
-
-try:
-    import mtots.test
-    mtots.test.case(_sample)
-    mtots.test.case(_sample2)
-except ImportError:
-    pass
+        })
