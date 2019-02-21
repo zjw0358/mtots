9f3f92b91a7c959079c8a92d1db61b2847de03e6
Kyumin Kim <math4tots@users.noreply.github.com>
2019-02-20 23:51:17 -0800

    nc ast: distinct annotation phase

--- mtots/text/nc/ast.py
+++ mtots/text/nc/ast.py
@@ -1,6 +1,7 @@
 from .rstates import NoReturn
 from .rstates import Returns
 from .rstates import ReturnState
+from .scopes import Scope
 from .types import Type
 from mtots import util
 from mtots.text import base
@@ -103,27 +104,72 @@ class FunctionDefinition(FunctionDeclaration, Definition):
     body: 'Block'
 
 
-class Statement(Node, metaclass=abc.ABCMeta):
-    _rstates = None  # return states
+class StatementOrExpression(Node, metaclass=abc.ABCMeta):
 
-    @property
-    def rstates(self) -> typing.Set[ReturnState]:
+    @classmethod
+    def _recursively_annotate(cls, node, scope):
+        if isinstance(node, Node):
+            if isinstance(node, Block):
+                inner_scope = Scope(scope)
+            else:
+                inner_scope = scope
+
+            for field_name in type(node).__dataclass_fields__:
+                field = getattr(node, field_name)
+                cls._recursively_annotate(field, inner_scope)
+
+            if isinstance(node, (Statement, Expression)):
+                node._annotate(inner_scope)
+
+        elif isinstance(node, (list, tuple, set, frozenset)):
+            for child in node:
+                cls._recursively_annotate(child, scope)
+
+        elif isinstance(node, dict):
+            for key, value in node.items():
+                cls._recursively_annotate(key, scope)
+                cls._recursively_annotate(value, scope)
+
+        elif node is None or isinstance(node, (bool, int, float, str, Type)):
+            # If we see a scalar, nothing to do
+            pass
+
+        else:
+            raise TypeError(node)
+
+    def annotate(self, scope):
+        StatementOrExpression._recursively_annotate(self, scope)
+
+
+class Statement(StatementOrExpression):
+    _rstates = None  # return states (Set[ReturnState])
+
+    def _annotate(self, scope):
         if self._rstates is None:
-            self._rstates = frozenset(self._get_return_states())
-        return self._rstates
+            self._rstates = frozenset(self._get_return_states(scope))
 
     @abc.abstractmethod
-    def _get_return_states(self):
+    def _get_return_states(self, scope):
         pass
 
+    @property
+    def rstates(self):
+        if self._rstates is None:
+            raise TypeError(f'.rstates accessed before annotate called')
+        return self._rstates
 
-class Expression(Node, metaclass=abc.ABCMeta):
+
+class Expression(StatementOrExpression):
     _type = None
+
+    def _annotate(self, scope):
+        if self._type is None:
+            self._type = self._get_type(scope)
 
     @property
     def type(self):
         if self._type is None:
-            self._type = self._get_type()
+            raise TypeError(f'.type accessed before annotate called')
         return self._type
 
     @abc.abstractmethod
@@ -135,7 +181,7 @@ class Expression(Node, metaclass=abc.ABCMeta):
 class ExpressionStatement(Statement):
     expr: Expression
 
-    def _get_return_states(self):
+    def _get_return_states(self, scope):
         return {NoReturn()}
 
 
@@ -145,7 +191,7 @@ class If(Statement):
     body: Statement
     other: typing.Optional[Statement]
 
-    def _get_return_states(self):
+    def _get_return_states(self, scope):
         return (
             self.body.rstates |
             (self.other.rstates if self.other else {NoReturn()})
@@ -157,7 +203,7 @@ class While(Statement):
     cond: Expression
     body: Statement
 
-    def _get_return_states(self):
+    def _get_return_states(self, scope):
         return {NoReturn()} | self.body.rstates
 
 
@@ -165,7 +211,7 @@ class While(Statement):
 class Return(Statement):
     expr: typing.Optional[Expression]
 
-    def _get_return_states(self):
+    def _get_return_states(self, scope):
         return {Returns(self.expr.type)}
 
 
@@ -173,7 +219,10 @@ class Return(Statement):
 class Block(Statement):
     stmts: typing.List[Statement]
 
-    def _get_return_states(self):
+    def _get_return_states(self, scope):
+        for stmt in self.stmts:
+            stmt.annotate(scope)
+
         nr = NoReturn()
         stmts = self.stmts
         rstates = {nr}
