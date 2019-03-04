32113f41e069e029041557d57c7f35f957647a5b
Kyumin Kim <math4tots@users.noreply.github.com>
2019-03-03 16:32:03 -0800

    Update loader.py

--- mtots/text/nc/loader.py
+++ mtots/text/nc/loader.py
@@ -1,3 +1,7 @@
+from . import cst
+from . import parser
+from mtots.text import base
+import collections
 import os
 
 _source_root = os.path.join(
@@ -11,3 +15,29 @@ def _import_path_to_file_path(import_path):
         _source_root,
         import_path.replace('.', os.path.sep) + '.nc',
     )
+
+
+def _parse_module(import_path):
+    file_path = _import_path_to_file_path(import_path)
+    with open(file_path) as f:
+        data = f.read()
+    return parser.parse(data=data, file_path=file_path)
+
+
+def load(data, *, file_path='<string>'):
+    main_tu = parser.parse(data=data, file_path=file_path)
+    seen = {'MAIN'}
+    pairs = []
+    queue = [('MAIN', main_tu)]
+    while queue:
+        tu_name, tu = queue.pop()
+        pairs.append((tu_name, tu))
+        for stmt in tu.stmts:
+            if isinstance(stmt, cst.Import):
+                import_path = stmt.path
+                if import_path not in seen:
+                    seen.add(import_path)
+                    import_tu = _parse_module(import_path)
+                    queue.append((import_path, import_tu))
+    return collections.OrderedDict(reversed(pairs))
+
